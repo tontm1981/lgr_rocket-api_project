7facc044741d72635659ef831c4caca41dfa4144
Elton Moreno <tontm1981@gmail.com>
2025-01-27 22:39:54 -0300

    stage2 - step1: done

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -5,22 +5,42 @@ use crate::models::*;
 // ---- CRUD for Questions ----
 
 #[post("/question", data = "<question>")]
+#[allow(unused_variables)]
 pub async fn create_question(
     question: Json<Question>,
 ) -> Json<QuestionDetail> {
-    todo!()
+    Json(QuestionDetail {
+        question_uuid: "question_id".to_string(),
+        title: "title".to_owned(),
+        description: "description".to_owned(),
+        created_at: "created_at".to_string(),
+    })
 }
 
 #[get("/questions")]
 pub async fn read_questions() -> Json<Vec<QuestionDetail>> {
-    todo!()
+    let questions = vec![
+        QuestionDetail {
+            question_uuid: "question_id".to_string(),
+            title: "title".to_owned(),
+            description: "description".to_string(),
+            created_at: "created_at".to_string(),
+        },
+        QuestionDetail {
+            question_uuid: "uuid2".to_string(),
+            title: "title2".to_string(),
+            description: "description 2".to_owned(),
+            created_at: "created_at".to_string(),
+        }
+    ];
+    Json(questions)
 }
 
 #[delete("/question", data = "<question_uuid>")]
+#[allow(unused_variables)]
 pub async fn delete_question(
     question_uuid: Json<QuestionId>
 ) {
-    todo!()
 }
 
 // ---- CRUD for Answers ----
@@ -29,13 +49,41 @@ pub async fn delete_question(
 //       The handler function should be called `create_answer`.
 //
 //       hint: this function should look very similar to the create_question function above
+#[post("/answer", data = "<answer>")]
+#[allow(unused_variables)]
+pub async fn create_answer( answer: Json<Answer> ) -> Json<AnswerDetail> {
+    Json(AnswerDetail {
+        question_uuid: "question_uuid".to_string(),
+        answer_uuid: "answer_uuid".to_string(),
+        content: "content".to_owned(),
+        created_at: "created_at".to_string(),
+    })
+}
 
 // TODO: Create a GET route to /answers which accepts an `QuestionId` and returns a vector of `AnswerDetail` as JSON.
 //       The handler function should be called `read_answers`.
 //
 //       hint: this function should look very similar to the read_questions function above
+#[get("/answers/<question_id>")]
+#[allow(unused_variables)]
+pub async fn read_answers( question_id: String) -> Json<Vec<AnswerDetail>> {
+    let answers = vec![
+        AnswerDetail {
+            question_uuid: "question_uuid".to_owned(),
+            answer_uuid: "answer_uuid".to_owned(),
+            content: "content".to_owned(),
+            created_at: "created_at".to_string(),
+        },
+    ];
+    Json(answers)
+}
 
 // TODO: Create a DELETE route to /answer which accepts an `AnswerId` and does not return anything.
 //       The handler function should be called `delete_answer`.
 //
 //       hint: this function should look very similar to the delete_question function above
+#[delete("/answer", data="<answer_id>")]
+#[allow(unused_variables)]
+pub async fn delete_answer( answer_id: String ) {
+    todo!()
+}
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,41 @@ mod cors;
 mod handlers;
 mod models;
 
+use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
 use cors::*;
 use handlers::*;
 
 #[launch]
 async fn rocket() -> _ {
+    // TODO: Initialize pretty_env_logger
+    pretty_env_logger::init();
+    // TODO: Initialize dotenv
+    dotenv().expect("Unable to initialize dotenv");
+
+    // Create a new PgPoolOptions instance with a maximum of 5 connections.
+    // Use dotenv to get the database url.
+    // Use the `unwrap` or `expect` method instead of handling errors. If an
+    // error occurs at this stage the server should be terminated.
+    // See examples on GitHub page: https://github.com/launchbadge/sqlx
+    // let pool = todo!();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://lgr:123456@localhost:5431/lgr")
+        .await
+        .expect("Unable to connect to database");
+
+    // Using slqx, execute a SQL query that selects all questions from the questions table.
+    // Use the `unwrap` or `expect` method to handle errors. This is just some test code to
+    // make sure we can connect to the database.
+    // let recs = todo!();
+    let recs = sqlx::query!("SELECT * FROM public.questions")
+        .fetch_all(&pool)
+        .await
+        .expect("Unable to fetch records");
+
+    info!(" ****************** Question Records ****************** ");
+    info!("{recs:?}");
     rocket::build()
         .mount(
             "/",
